00f6e913523f68254ee28a456f1d5f880c04de38
Danielle Dempsey <ddempsey@perennia.ca>
2024-04-23 08:52:40 -0300

    modify to reflect updated official station locations sheet

--- log_compilation_and_analysis/station_location_check.R
+++ log_compilation_and_analysis/station_location_check.R
@@ -10,19 +10,17 @@ library(sensorstrings)
 library(sf)
 
 
-not_in_area_info <- c(
-  "Broad 1 Dirt Road 1",
-  "Medway 1 Bangs Falls 1",
-  "Medway 2 Big Cove 1",
-  "Medway 3 Greenfield 1",
-  "Mersey 1 River Road Offshoot 1",
-  "Mersey 2 River Road 2",
-  "Mersey 4 Fish Hatchery Road 1"
-  )
+# not_in_area_info <- c(
+#   "Broad 1 Dirt Road 1",
+#   "Medway 1 Bangs Falls 1",
+#   "Medway 2 Big Cove 1",
+#   "Medway 3 Greenfield 1",
+#   "Mersey 1 River Road Offshoot 1",
+#   "Mersey 2 River Road 2",
+#   "Mersey 4 Fish Hatchery Road 1"
+#   )
 
-dat_all <- readRDS(
-  here("log_compilation_and_analysis/stacked_logs_2024-04-15.rds")
-)
+dat_all <- readRDS(here("stacked_logs_2024-04-17.rds"))
 
 dat <- dat_all %>%
   select(
@@ -34,8 +32,8 @@ dat <- dat_all %>%
     retrieval_latitude,
     retrieval_longitude
   ) %>%
-  distinct() %>%
-  filter(!(station %in% not_in_area_info))
+  distinct()# %>%
+  #filter(!(station %in% not_in_area_info))
 
 # link to the "STRING TRACKING" google sheet
 googlesheets4::gs4_deauth()
@@ -43,7 +41,7 @@ googlesheets4::gs4_deauth()
 link <- "http://docs.google.com/spreadsheets/d/1a3QvJsvwr4dd64g3jxgewRtfutIpsKjT2yrMEAoxA3I/edit#gid=828367890"
 
 # read in the "Area Info" tab of the STRING TRACKING sheet
-station_coords <- googlesheets4::read_sheet(link, sheet = "Area Info") %>%
+station_coords <- googlesheets4::read_sheet(link, sheet = "Area Info2") %>%
   select(station, latitude, longitude) %>%
   filter(!is.na(latitude), !is.na(longitude))
 
@@ -74,6 +72,7 @@ for(i in seq_along(1:nrow(dat))) {
   ocean_check <- ss_check_station_in_ocean(log_i, coast_shp = ns)
 
   location_check[[i]] <- data.frame(log_i, rad_check, drift_check, ocean_check)
+  #location_check[[i]] <- data.frame(log_i, rad_check, drift_check)
 
   if(i %% 20 == 0) print(i)
 }
@@ -84,8 +83,47 @@ location_check <- location_check %>%
 
 saveRDS(
   location_check,
-  here("log_compilation_and_analysis/station_location_checks.rds")
+  here("2024-04-17_station_location_check.rds")
 )
 
 
+# drift analysis  ----------------------------------------------------------------
+library(ggplot2)
 
+drift_dist <- list()
+
+for(i in seq_along(1:nrow(dat))) {
+  
+  log_i <- dat[i, ]
+  
+  if(!is.na(log_i$retrieval_latitude) & !is.na(log_i$retrieval_longitude)) {
+    drift_check <- ss_check_station_drift(log_i, return_drift = TRUE)
+  } else  drift_check <- NA
+  
+  drift_dist[[i]] <- data.frame(log_i, drift_check)
+  
+  if(i %% 20 == 0) print(i)
+}
+
+drift_dist <- drift_dist %>%
+  list_rbind()
+
+
+x <- drift_dist %>% 
+  filter(!is.na(drift_check)) %>% 
+  summarise(
+    min = min(drift_check),
+    max = max(drift_check),
+    mean = mean(drift_check),
+    med = median(drift_check)
+  )
+
+
+ggplot(drift_dist, aes(drift_check)) +
+  geom_histogram()
+
+
+
+# Look into ---------------------------------------------------------------
+
+dat <- readRDS(here("2024-04-17_station_location_check.rds"))
